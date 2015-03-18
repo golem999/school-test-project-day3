0dd897daad1b2bb8b96504c3de0805054ad4bb45
SamhainG <gavrilyk.oleg@gmail.com>
2015-03-19 01:13:16 +0200

    First commit

--- controllers/helloController.js
+++ controllers/helloController.js
@@ -1,23 +1,50 @@
-var ect = require("ect");
-var renderer = ect({ root : __dirname + '/../views' });
-var data = { title : 'Hello, World!' };
+var users = require("../lib/users");
+var url = require("url");
 
 module.exports = {
-   getAction: function(request, response, next){
-       /* next(null) */
-       setTimeout(function(next){
-           response.statusCode = 200;
-           try{
-               response.write(renderer.render('hello.ect', {
-                   message: "Hello world"
-               }));
-               next();
-           }catch(e){
-               next(e);
-           }
-       }, 500, next)
-   },
-    postAction: function(request, response, next){
+    "GET": function (request, response, callback) {
+
+        var parsedUrl = url.parse(request.url, true);
+
+        parsedUrl = parsedUrl.href.replace("/", "");
+        if (parsedUrl === "users" || parsedUrl === "users.html") {
+            var userList = users.getAllUsers;
+            var i = 0;
+            response.write("users:\n");
+            for (i = 0; i < userList.length; i++) {
+                response.write(
+                    "User" + (i + 1) + ": {" +
+                    "username: " + userList[i].username +
+                    ", e-mail: " + userList[i].mail +
+                    ", description: " + userList[i].description +
+                    ", age: " + userList[i].age +
+                    "}\n");
+
+            }
+            response.statusCode = 200;
+            response.end("");
+        }
+        else {
+            response.statusCode = 404;
+            response.end("404 page not found");
+        }
+
+
+    },
+
+    "POST": function (request, response, callback) {
+
+        var query = "";
+        request.on('data', function (data) {
+            query += data;
+        });
+        request.on('end', function () {
+            var newUser = users.parseJSON(query);
+            users.addUser(newUser);
+            response.end();
+        });
 
     }
+
+
 };
